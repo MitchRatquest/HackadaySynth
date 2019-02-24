a88a4e4130851ec2cbad6b1444d51debb496b524
Mitchell Dokken <Mitchell.Dokken@gmail.com>
2019-02-24 15:30:28 -0600

    Clean up and rearrange Things

--- configs/overlay/root/notes/get_cpu.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}'
-free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
-df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$5}'
