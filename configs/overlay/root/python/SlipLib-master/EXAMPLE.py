5d2af6197db9a3515b2a34e99c518e126c3a5bd3
Mitchell Dokken <Mitchell.Dokken@gmail.com>
2019-03-05 17:00:37 -0600

    remove old things

--- configs/overlay/root/python/SlipLib-master/EXAMPLE.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-import serial, sliplib
-com = serial.Serial('/dev/ttyS0',115200)
-driver = sliplib.Driver() 
-message = bytes("hello my friend")
-com.write(driver.send(message))
-com.close()
