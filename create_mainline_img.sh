a11de4db20baa56716ba107a507309585c91d3ce
MitchRatquest <mitchell.dokken@gmail.com>
2019-08-10 13:00:28 -0500

    Use rockchip defconfig

--- create_mainline_img.sh
+++ create_mainline_img.sh
@@ -6,11 +6,15 @@ then
 	git clone https://github.com/rockchip-linux/buildroot.git rockchip-buildroot
 fi
 
-cd rockchip-buildroot
-git checkout "$VERSION"
-patch -p1 < ../patches/buildroot/0000-add-pd-tk.patch
-cd ..
+if [ ! -f .br_patched ]
+then
+	cd rockchip-buildroot
+	git checkout "$VERSION"
+	patch -p1 < ../patches/buildroot/0000-add-pd-tk.patch
+	cd ..
+	touch .br_patched
+fi
 
 
-make O=$PWD -C rockchip-buildroot/ defconfig BR2_DEFCONFIG=../configs/buildroot_defconfig
-make
+make O=$PWD -C rockchip-buildroot/  defconfig BR2_DEFCONFIG=../configs/br_rk_defconfig
+#make
