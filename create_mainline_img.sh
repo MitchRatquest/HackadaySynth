40ccb2d6a316bd8f6f707d5334d7a0beef575d6c
Mitchell Dokken <Mitchell.Dokken@gmail.com>
2020-03-15 11:59:08 -0500

    Update buildroot and patches to latest

--- create_mainline_img.sh
+++ create_mainline_img.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=2019.11
+VERSION=2020.02
 
 if [ ! -d buildroot-"$VERSION" ]
 then
@@ -8,10 +8,13 @@ then
 fi
 
 #extract custom packages to buildroot's directory
-tar xvf patches/buildroot/externals.tar.gz -C `pwd`/buildroot-"$VERSION"/package/
+#tar xvf patches/buildroot/externals.tar.gz -C `pwd`/buildroot-"$VERSION"/package/
 
 #apply patches to buildroot
 cd buildroot-"$VERSION"
+patch -p1 < ../patches/buildroot/0000-puredata.patch
+patch -p1 < ../patches/buildroot/0001-add-trx.patch
+patch -p1 < ../patches/buildroot/0002-orca.patch
 patch -p1 < ../patches/buildroot/0003-ssh-root-x11.patch
 patch -p1 < ../patches/buildroot/0004-package-config.patch
 patch -p1 < ../patches/buildroot/genimage_rootfs_size.patch
